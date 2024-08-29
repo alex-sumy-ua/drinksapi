f4f0f3fd3707f77b2c437dd03b49682108ddb66d
alex-sumy-ua <plachkovskyy@yahoo.com>
2024-08-29 13:54:27 +0100

    Fixed the last Warnings

---
+++ src/main/java/com/northcoders/drinksapi/controller/CoffeeLoverController.java
@@ -0,0 +1,15 @@
+package com.northcoders.drinksapi.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/coffeelover")
+public class CoffeeLoverController {
+
+    @GetMapping
+    public String getCoffeeLoverMessage() {
+        return "I Like Coffee!";
+    }
+}
